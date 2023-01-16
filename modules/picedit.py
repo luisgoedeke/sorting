bf62d01ea5fc0494d78c10b07765d5d3be36268d
Luis Gödeke <luis.goedeke@gmail.com>
2023-01-16 14:08:56 +0100

    Update picedit.py

--- modules/picedit.py
+++ modules/picedit.py
@@ -2,47 +2,46 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 from skimage import color
-from skimage import io
-
-#path='C:/Users/Luis/Pictures/rainbow.jpg'
-
-#img = cv2.imread(path)
-
-#plt.imshow(img)
-
-#img = cv2.imread(path,0)
-
-#cv2.imwrite('C:/Users/Luis/Pictures/rainbowblack.jpg',img)
-
-#ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-
-#cv2.imwrite('C:/Users/Luis/Pictures/rainbowblack.jpg',thresh1)
  
-#source = 'C:/Users/Luis/Pictures/Bilder'
+source = 'C:/Users/Luis/Pictures/Bilder'
 
-#destination = 'C:/Users/Luis/Pictures/Bilder3'
+destination = 'C:/Users/Luis/Pictures/Bilder_bearbeitet'
 
-#test=1
+test=1
 
-#for dir1 in os.listdir(source):
-    #for dir2 in os.listdir(os.path.join(source, dir1)):
-        #os.makedirs(os.path.join(destination, dir1,dir2))
-        #for dir3 in os.listdir(os.path.join(source, dir1,dir2)):
-            #path=os.path.join(source, dir1,dir2,dir3)
-            #print(path)
-            #pic=os.path.join(destination, dir1,dir2,dir3)
-            #img = cv2.imread(path,0)
-            #cv2.imwrite(pic,img)
-            #print(test)
-            #test=test+1
+for dir1 in os.listdir(source):
+    for dir2 in os.listdir(os.path.join(source, dir1)):
+        os.makedirs(os.path.join(destination, dir1,dir2))
+        for dir3 in os.listdir(os.path.join(source, dir1,dir2)):
+            spath=os.path.join(source, dir1,dir2,dir3) #Quellpfad
+            print(spath)
+            dpath=os.path.join(destination, dir1,dir2,dir3) #Zielpfad
+            img = cv2.imread(spath) #Bild einlesen
+            img = img[0:480, 104:584] #Bild zuschneiden
 
-img = cv2.imread('C:/Users/Luis/Pictures/test.jpg')
-img = img[0:480, 104:584]
-gamma = 0.95
-img = np.power(img, gamma)
-imgGray = color.rgb2gray(img)
-#image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#ret,img = cv2.threshold(img,150,255,cv2.THRESH_TRUNC)
-cv2.imwrite('C:/Users/Luis/Pictures/test1.jpg',imgGray)
+            lab= cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+            l_channel, a, b = cv2.split(lab)
+
+            # Applying CLAHE to L-channel
+            # feel free to try different values for the limit and grid size:
+            clahe = cv2.createCLAHE(clipLimit=2.7, tileGridSize=(8,8))
+            cl = clahe.apply(l_channel)
+
+            # merge the CLAHE enhanced L-channel with the a and b channel
+            limg = cv2.merge((cl,a,b))
+
+            # Converting image from LAB Color model to BGR color spcae
+            enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+
+            #Geliehen von https://stackoverflow.com/questions/39308030/how-do-i-increase-the-contrast-of-an-image-in-python-opencv
+
+
+            gamma = 0.95
+            img = np.power(enhanced_img, gamma)
+            imgGray = color.rgb2gray(img)
+            cv2.imwrite(dpath,imgGray)
+
+            print(test)
+            test=test+1
+
