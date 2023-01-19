331fb83c51cdf4a054297868875f3a35f3c87f88
Luis Gödeke <luis.goedeke@gmail.com>
2023-01-19 18:07:48 +0100

    arduino update

--- sketch_nov11a/sketch_nov11a.ino
+++ sketch_nov11a/sketch_nov11a.ino
@@ -1,6 +1,89 @@
-int Distance = 0; String command;// Record the number of steps we've taken void setup() {
+int Distance = 0;
+String command;
+bool run_allowed = false;
+bool full_rot_allowed = false;
+int delaytime = 500;
+int steps = 1200;
+int times_angle_rot = 0;
+int times_full_rot = 0;
+int counter_full_rot = 0;
+int counter_angle_rot = 0;
+
+void schritt_rl(){
+
+    if (digitalRead(9) == LOW) {
+      digitalWrite(9, HIGH);
+    }
+        
+    digitalWrite(8, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(8, LOW);
+    delayMicroseconds(100);
+  }
+
+void schritt_ll(){
+    if (digitalRead(9) == HIGH) {
+      digitalWrite(9, LOW);
+    }
+    digitalWrite(8, HIGH);
+    delayMicroseconds(100);
+    digitalWrite(8, LOW);
+    delayMicroseconds(100);
+  
+}
+
+void serial(){
+  if (Serial.available()){
+
+    command = Serial.readStringUntil('\n');
+    command.trim();
+  }
+
+    if (command.equals("start")){
+    run_allowed = true;
+    command = "";
+  }
+
+    if (command.equals("stop")){
+    run_allowed = false;
+    command = "";
+  }
+
+    if (command.startsWith("allow_full_rot")){
+     full_rot_allowed = true;
+     command = "";
+  }
+
+    if(command.startsWith("n_allow_full_rot")){
+      full_rot_allowed = false;
+      command = "";
+    }
+
+    if (command.startsWith("steps")){
+    command.remove(0,5);
+    steps = command.toInt();
+    command = "";
+  }
+
+      if (command.startsWith("delay")){
+    command.remove(0,5);
+    delaytime = command.toInt();
+    command = "";
+  }
+
+      if (command.startsWith("times_full_rot")){
+    command.remove(0,14);
+    times_full_rot = command.toInt();
+    command = "";
+  }
+}
+
 void setup(){
   Serial.begin(9600);
+
+  Serial.println("Test");
+
+  pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(8, OUTPUT);
 
@@ -16,46 +99,50 @@ void setup(){
 
 void loop() {
 
-  if (Serial.available()){
+  serial();
+  if (run_allowed){
+    if (full_rot_allowed){
+      for(int i=0; i<steps;i++){
+        schritt_rl();
+        Serial.println("RL");
+      }
+      
+      delay(delaytime);
+      
+      for(int i=0; i<steps;i++){
+        schritt_ll();
+        Serial.println("LL");
+      }
+      
+      delay(delaytime);
+      
+      for(int i=0; i<times_full_rot;i++){
+        schritt_rl();
+        Serial.println("RLFull");
+      }
 
-    command = Serial.readStringUntil('\n');
-    command.trim();
+      delay(delaytime);
+
+      for(int i=0; i<times_full_rot;i++){
+        schritt_ll();
+        Serial.println("LLFull");
+      }
+      
+    }else{
+       for(int i=1; i<=steps; i++){
+        schritt_rl();
+        Serial.println("RL");
+      }
+      
+      delay(delaytime);
+      
+      for(int i=1; i<=steps; i++){
+        schritt_ll();
+        Serial.println("LL");
+      }
+      delay(delaytime);
+    }
   }
-
-  if (command.equals("start")){
-
-    digitalWrite(8, HIGH);
-
-    delayMicroseconds(100);
-
-    digitalWrite(8, LOW);
-
-    delayMicroseconds(100);
-
-
-    Distance = Distance + 1; // record this step // Check to see if we are at the end of our move
-
-    // two rotation for 1/8 bridge and 1 rotation for 1/6 bridge (for this code)
-
-    if (Distance == 1200) { // We are! Reverse direction (invert DIR signal)
-      delay(500);
-
-      if (digitalRead(9) == LOW) {
-
-        digitalWrite(9, HIGH);
-       
-      }else {
-
-        digitalWrite(9, LOW);
-
-      } // Reset our distance back to zero since we're // starting a new move
-
-      Distance = 0; // Now pause for half a second delay(500);
-
-  }
-    
-  }
-
 
 
 
