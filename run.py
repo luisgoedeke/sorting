94f28889c58b8ebb9fa0e1c77a415fa4d4518047
luis.goedeke@gmail.com <luis.goedeke@gmail.com>
2023-01-24 17:08:49 +0100

    GUI läuft

--- run.py
+++ run.py
@@ -18,21 +18,12 @@ from threading import Thread
 m = Machine()
 sorting = Sorting(m)
 
-#Schleife zum ständigen wiederholen des Hauptmenues
-
-#deckel1 = Kunststoff()
-#deckel2 = Metall()
-#deckel3 = Kronkorken()
-#print(deckel1.get_pos_soll())
-#print(deckel2.get_pos_soll())
-#print(deckel3.get_pos_soll())
-
 root = Tk()
 
 button_func = Button(root, text="Funktionstest", width=40, height=2, font=120, bg="purple", command=m.ea_test)
 button_take_pictures = Button(root, text="Bilder aufnehmen", width=40, height=2, font=120, bg="purple", command=m.take_pictures)
-button_ver_start = Button(root, text="Vereinzelung starten", width=40, height=2, font=120, bg="green", command=m.ver_start)
-button_ver_stop = Button(root, text="Vereinzelung stoppen", width=40, height=2, font=120, bg="red", command=m.ver_stop)
+button_ver_start = Button(root, text="Vereinzelung starten", width=40, height=2, font=120, bg="green", command=m.start_ver)
+button_ver_stop = Button(root, text="Vereinzelung stoppen", width=40, height=2, font=120, bg="red", command=m.stop_ver)
 button_para_ver = Button(root, text="Vereinzelung Parameter", width=40, height=2, font=120, bg="yellow", command=m.param_ver)
 button_belt_start = Button(root, text="Band starten", width=40, height=2, font=120, bg="green", command=m.start_belt_rl)
 button_belt_stop = Button(root, text="Band stoppen", width=40, height=2, font=120, bg="red", command=m.stop_belt)
@@ -50,35 +41,5 @@ button_sort_start.pack()
 
 root.mainloop()
 
-while True:
-
-    auswahl = input("Willkommen im Hauptmenue! \n Bitte waehlen sie eine Option: \n 1: Starten der Vereinzelung \n 2: Stoppen der Vereinzelung \n 3: Funktionstest der Maschine starten \n 4: Bilder aufnehmen \n 5: Sortierung starten \n 6: Parameter der Vereinzelung anpassen \n 7: Band starten \n 8: Band stoppen \n")
-
-    if auswahl == "1":
-        m.start_ver()
-
-    elif auswahl == "2":
-        m.stop_ver()
-
-    elif auswahl == "3":
-        m.ea_test()
-
-    elif auswahl == "4":
-        m.take_pictures()
-        
-    elif auswahl == "5":
-        sorting.sort()
-
-    elif auswahl == "6":
-        m.param_ver()
-    
-    elif auswahl == "7":
-        m.start_belt_rl()
-        
-    elif auswahl == "8":
-        m.stop_belt()
-
-    else:
-        print("Sie haben keine gueltige Eingabe getroffen.")
         
 
